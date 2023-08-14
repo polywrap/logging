2e1a06fd2d400a3d2b5505a0d859a78254c130a2
krisbitney <kris@dorg.tech>
2023-08-14 16:34:43 -0500

    fixed publishing and dependencies

--- logger/implementations/kt/build.gradle.kts
+++ logger/implementations/kt/build.gradle.kts
@@ -1,37 +1,62 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    id("com.android.library") version "8.2"
+    kotlin("multiplatform") version "1.9.0"
     kotlin("plugin.serialization") version "1.9.0"
     id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
     id("org.jetbrains.dokka") version "1.8.20"
     id("convention.publication")
 }
 
-group = "io.polywrap"
+group = "io.polywrap.plugins"
 version = "0.10.0-SNAPSHOT"
 
 repositories {
+    google()
     mavenCentral()
-    maven {
-        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-    }
-}
-
-tasks.test {
-    useJUnitPlatform()
+    maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvm {
+        jvmToolchain(17)
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    androidTarget {
+        publishLibraryVariants("release")
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("io.polywrap:polywrap-client:0.10.0-SNAPSHOT")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+                implementation("org.slf4j:slf4j-api:2.0.7")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+                implementation("ch.qos.logback:logback-classic:1.3.5")
+            }
+        }
+    }
 }
 
-dependencies {
-    implementation("io.polywrap:polywrap-client:0.10.0-SNAPSHOT")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.slf4j:slf4j-api:2.0.7")
-    testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    testImplementation("ch.qos.logback:logback-classic:1.3.5")
+android {
+    namespace = "io.polywrap.plugins.logger"
+    compileSdk = 32
+    defaultConfig.minSdk = 24
+    compileOptions {
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    testOptions {
+        unitTests.all {
+            it.enabled = false
+        }
+    }
 }
 
 // javadoc generation for Maven repository publication
--- logger/implementations/kt/src/commonTest/kotlin/io/polywrap/plugins/logger/HelloWorld.kt
+++ logger/implementations/kt/src/commonTest/kotlin/io/polywrap/plugins/logger/HelloWorld.kt
@@ -1,16 +1,20 @@
+package io.polywrap.plugins.logger
+
 import io.polywrap.configBuilder.polywrapClient
 import io.polywrap.core.InvokeResult
 import io.polywrap.core.resolution.Uri
-import io.polywrap.plugins.logger.loggerPlugin
 import io.polywrap.plugins.logger.wrap.ArgsLog
 import io.polywrap.plugins.logger.wrap.LogLevel
+import kotlinx.serialization.Serializable
 import kotlin.test.Test
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class HelloWorld {
 
+    val aggregatorWrapUri = "wrap://http/https://wraps.wrapscan.io/r/polywrap/logging@1.0.0"
     val interfaceUri = "wrapscan.io/polywrap/logger@1.0"
+
     val client = polywrapClient {
         addDefaults()
         setPackage("plugin/logger" to loggerPlugin(null))
@@ -26,6 +30,23 @@ class HelloWorld {
             uri = Uri(interfaceUri),
             method = "log",
             args = ArgsLog(LogLevel.INFO, "Hello, World!")
+        )
+
+        assertNull(result.exceptionOrNull())
+        assertTrue(result.getOrThrow())
+    }
+
+    @Test
+    fun printHelloWorldThroughAggregatorWrap() {
+        @Serializable
+        class ArgsInfo(val message: String)
+
+        println("Invoking Method: info")
+
+        val result: InvokeResult<Boolean> = client.invoke(
+            uri = Uri(aggregatorWrapUri),
+            method = "info",
+            args = ArgsInfo("Hello, World!")
         )
 
         assertNull(result.exceptionOrNull())
